d0ca4728b109a402b4875e4975aaf418118d53cd
Ryan Govostes <rgovostes+git@gmail.com>
2017-12-19 04:43:08 +0900

    Add tests for flags and extended attributes

--- Lib/test/test__copyfile.py
+++ Lib/test/test__copyfile.py
@@ -3,6 +3,7 @@ The goal of the _copyfile extension module is to mimic the behavior of shutil's
 family of copy functions (insofar as they are sensible).
 '''
 import _copyfile
+import errno
 import os
 import stat
 import sys
@@ -17,7 +18,7 @@ import unittest
 import shutil
 
 
-# Set to True and the temporary files will not be destroyed 
+# Set to True and the temporary files will not be destroyed
 DEBUG = False
 
 
@@ -81,7 +82,7 @@ class FileGenerator(object):
     def create_socket(self):
         raise NotImplementedError()
 
-# Helper methods that check various file metadata
+# Helper methods that operate on various kinds of file metadata
 
 def is_regular_file(path):
     # Be sure to use os.lstat, so that we don't follow links!
@@ -90,7 +91,49 @@ def is_regular_file(path):
 def is_symlink(path):
     return stat.S_ISLNK(os.lstat(path).st_mode)
 
+def get_file_mode(path):
+    return stat.S_IMODE(os.lstat(path).st_mode)
+
+
 # Unit tests
+
+@unittest.skipUnless(sys.platform == 'darwin', 'requires macOS')
+class XattrTestCase(unittest.TestCase):
+    # XXX
+    # The os module's family of xattr functions do not work on macOS. (The
+    # 'xattr' module that Apple distributes with macOS is not part of the
+    # standard distribution.)
+    #
+    # Therefore, the _copyfile module provides functions for reading and writing
+    # extended attributes ONLY for testing purposes.
+
+    @classmethod
+    def setUpClass(cls):
+        cls.fg = FileGenerator()
+
+    @classmethod
+    def tearDownClass(cls):
+        if DEBUG:
+            print()
+            print('Keeping working directory for', cls.__name__)
+            print('  ' + cls.fg.tempdir)
+        else:
+            cls.fg.destroy()
+        cls.fg = None
+
+    def test_xattr_readwrite(self):
+        name, value = 'org.python._copyfile.test', b'hello world'
+        f = self.fg.create_file()
+        _copyfile._setxattr(f, name, value)
+        self.assertEqual(_copyfile._getxattr(f, name), value)
+
+    def test_xattr_read_nonexistent(self):
+        name = 'org.python._copyfile.test'
+        f = self.fg.create_file()
+        with self.assertRaises(OSError) as cm:
+            _copyfile._getxattr(f, name)
+        self.assertEqual(cm.exception.errno, errno.ENOATTR)
+
 
 @unittest.skipUnless(sys.platform == 'darwin', 'requires macOS')
 class CopyfileTestCase(unittest.TestCase):
@@ -167,3 +210,41 @@ class CopyfileTestCase(unittest.TestCase):
             f.write('hello world')
         _copyfile.copyfile(src, dst)
         self.assertFalse(os.path.exists(rsrcfork(dst)))
+
+    def test_copy_mode(self):
+        src = self.fg.create_file()
+        dst = self.fg.create_filename()
+        mode = get_file_mode(src) ^ stat.S_IWGRP  # flip some bit
+        os.chmod(src, mode)
+        _copyfile.copyfile(src, dst)
+        self.assertNotEqual(get_file_mode(dst), mode)
+
+    def test_copy_flags(self):
+        src = self.fg.create_file()
+        dst = self.fg.create_filename()
+        flags = os.lstat(src).st_flags ^ stat.UF_HIDDEN # flip some flag
+        os.lchflags(src, flags)
+        _copyfile.copyfile(src, dst)
+        self.assertNotEqual(os.lstat(dst).st_flags, flags)
+
+    def test_copy_restricted_flags(self):
+        # As of macOS 10.10, copyfile(3) will *not* copy the SF_RESTRICTED flag
+        # (unless the destination directory also has it set)
+        src = '/System/Library/CoreServices/SystemVersion.plist'
+        dst = self.fg.create_filename()
+        self.assertNotEqual(os.lstat(src).st_flags & stat.SF_RESTRICTED, 0)
+        _copyfile.copyfile(src, dst)
+        self.assertEqual(os.lstat(dst).st_flags & stat.SF_RESTRICTED, 0)
+
+    def test_copy_acls(self):
+        pass
+
+    def test_copy_xattrs(self):
+        name, value = 'org.python._copyfile.test', b'hello world'
+        src = self.fg.create_file()
+        dst = self.fg.create_filename()
+        _copyfile._setxattr(src, name, value)
+        _copyfile.copyfile(src, dst)
+        with self.assertRaises(OSError) as cm:
+            _copyfile._getxattr(dst, name)
+        self.assertEqual(cm.exception.errno, errno.ENOATTR)
